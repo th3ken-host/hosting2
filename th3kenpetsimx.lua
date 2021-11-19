2de78749fbd108e38806e1333fc4b8ea497e72fb
TH3KEN-DEV <39413139+th3ken-dev@users.noreply.github.com>
2021-11-19 22:01:45 +0300

    Update th3kenpetsimx.lua

--- th3kenpetsimx.lua
+++ th3kenpetsimx.lua
@@ -1,4 +1,4 @@
 --by th3ken
 print("TH3KEN By Loaded")
-_G.Key = "nukeplaysroblos" 
+_G.Key = "nukeplaysgrowtopia" 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NukeVsCity/TheALLHACKLoader/main/NukeLoader"))()
